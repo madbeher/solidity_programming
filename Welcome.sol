b888ec9fca805f7bc192c53c844fd119f248f96e
madan behera <madanbehera.1987@gmail.com>
2019-07-08 23:18:36 +0530

    initial commit

---
+++ Welcome.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.4.2 <0.6.0;
+
+contract Welcome
+{
+    string private message = "";
+    function outputMessage() public pure returns(string memory)
+    {
+        return "i am learing solidity";
+    }
+}   
+
